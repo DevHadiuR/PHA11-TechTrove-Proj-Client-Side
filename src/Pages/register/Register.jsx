45524cf3648b1b9607ca923444f1c9619844fd1b
Hadiur Rahman <hadiurahman139@gmail.com>
2024-05-13 10:53:04 +0600

    complete the funcitonility of the register page

--- src/Pages/register/Register.jsx
+++ src/Pages/register/Register.jsx
@@ -12,6 +12,9 @@ import "sweetalert2/src/sweetalert2.scss";
 import { Helmet } from "react-helmet-async";
 import useAuth from "../../hooks/useAuth";
 import { updateProfile } from "firebase/auth";
+import NavBar from "../../Shared/navbar/NavBar";
+
+import bg2 from "../../assets/authenticationBG/log2.jpg";
 
 const Register = () => {
   const [showPass, setShowPass] = useState(false);
@@ -68,7 +71,7 @@ const Register = () => {
     createUser(email, password)
       .then((res) => {
         const user = res.user;
-
+        console.log(user);
         Swal.fire({
           title: "Congratulation!",
           text: "You Have Successfully Registered!",
@@ -95,154 +98,158 @@ const Register = () => {
   };
 
   return (
-    <div className="hero min-h-screen bg-[#628E90]  py-24  mt-24">
-      <Helmet>
-        <title>Register Page | TechTrove</title>
-      </Helmet>
-      <div className="hero-content flex   gap-5 flex-col lg:flex-row  w-full">
-        <div className=" flex-1 w-full rounded-xl">
-          <div
-            className="hero min-h-screen rounded-xl"
-            style={{
-              backgroundImage: "url(/register.jpg)",
-            }}
-          >
-            <div className="hero-overlay bg-opacity-40 rounded-xl"></div>
-            <div className="hero-content text-center text-neutral-content">
-              <div className="max-w-lg">
-                <h1 className="mb-5 text-4xl md:text-5xl font-bold font-dm">
-                  Join the Community of Art Enthusiasts
-                </h1>
-                <p className="mb-5 font-serif">
-                  Welcome to the vibrant world of sculpting! Dive into a realm
-                  where creativity knows no bounds and imagination takes shape
-                  in the form of clay, stone, metal, and more. Whether
-                  you&apos;re a seasoned sculptor or just starting on your
-                  artistic journey, our registration page opens the door to a
-                  community of like-minded individuals passionate about
-                  sculpting.
-                  <br />
-                  <br />
-                  Unlock a plethora of resources, from expert tutorials and tips
-                  to engaging forums where you can share your latest creations
-                  and seek inspiration from fellow sculptors. Connect with
-                  artists from around the globe, each with their own unique
-                  style and perspective, and embark on a collaborative journey
-                  that celebrates the beauty of three-dimensional art. Join us
-                  today and become part of a supportive network that nurtures
-                  creativity, fosters growth, and celebrates the diverse
-                  artistry of sculpture. Sign up now to unleash your artistic
-                  potential and sculpt your dreams into reality!
-                </p>
+    <section className="relative z-0">
+      <div className="absolute w-full top-0 z-50    bg-transparent ">
+        <NavBar></NavBar>
+      </div>
+      <div className="hero min-h-screen bg-[#79BAC1]  py-36 ">
+        <Helmet>
+          <title>Register Page | TechTrove</title>
+        </Helmet>
+        <div className="hero-content flex   gap-5 flex-col lg:flex-row  w-full">
+          <div className=" flex-1 w-full rounded-xl ">
+            <div
+              className="hero rounded-xl"
+              style={{
+                backgroundImage: `url(${bg2})`,
+              }}
+            >
+              <div className="hero-overlay bg-opacity-40 rounded-xl"></div>
+              <div className="hero-content text-center text-neutral-content  ">
+                <div className="max-w-lg space-y-10 my-10">
+                  <h1 className="mb-5 text-4xl md:text-5xl font-bold text-white">
+                    &quot;Get Started: Create Your Account&quot;
+                  </h1>
+                  <p className="mb-5">
+                    &quot;Embark on your technological journey by registering
+                    for an account today. Take the first step towards accessing
+                    a vast repository of knowledge, connecting with a diverse
+                    community of tech enthusiasts, and staying informed about
+                    the latest innovations in the industry. With your
+                    registration, you&apos;ll unlock a plethora of exclusive
+                    features designed to enhance your experience, including
+                    personalized content recommendations tailored to your
+                    interests, engaging discussions with fellow members, access
+                    to premium resources, and the opportunity to contribute your
+                    insights and expertise to our growing platform. Join us in
+                    shaping the future of technology and be a part of our
+                    dynamic community committed to fostering learning,
+                    collaboration, and innovation.&quot;
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="card flex-1 w-full ">
-          <div className=" bg-[#F5EFE6] rounded-2xl">
-            <h1 className="text-center mb-2 mt-2 border-b-2 w-[70%] border-[#543A3A] mx-auto pb-6 text-5xl font-semibold font-dm text-[#3D3B40]">
-              REGISTER NOW!
-            </h1>
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="card-body font-serif"
-            >
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Name</span>
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Enter Your Name"
-                  className="input input-bordered border-[#543A3A]"
-                  {...register("name", { required: true })}
-                />
-                {errors.name && (
-                  <span className="mt-2 text-red-800">
-                    This field is required
-                  </span>
-                )}
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">URL</span>
-                </label>
-                <input
-                  type="url"
-                  name="url"
-                  placeholder="Enter Your Photo URL"
-                  className="input input-bordered border-[#543A3A]"
-                  {...register("url", { required: true })}
-                />
-                {errors.url && (
-                  <span className="mt-2 text-red-800">
-                    This field is required
-                  </span>
-                )}
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Enter Your Email"
-                  className="input input-bordered border-[#543A3A]"
-                  {...register("email", { required: true })}
-                />
-                {errors.email && (
-                  <span className="mt-2 text-red-800">
-                    This field is required
-                  </span>
-                )}
-              </div>
-              <div className="form-control relative">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type={showPass ? "text" : "password"}
-                  name="password"
-                  placeholder="Enter Your Password"
-                  className="input input-bordered border-[#543A3A]"
-                  {...register("password", { required: true })}
-                />
-                <span
-                  onClick={handleShowPassword}
-                  className="absolute bottom-3 right-5 text-2xl cursor-pointer"
-                >
-                  {showPass ? <IoMdEyeOff /> : <IoEye />}
-                </span>
-              </div>
-              {errors.password && (
-                <span className="mt-2 text-red-800">
-                  This field is required
-                </span>
-              )}
-              <div className="form-control mt-6">
-                <button
-                  type="submit"
-                  className="btn bg-[#628E90] text-lg hover:bg-[#3C2317] text-white transition-all"
-                >
-                  REGISTER
-                </button>
-                <ToastContainer />
-              </div>
-            </form>
-            <div className="mb-5">
-              <h1 className="text-center font-serif text-lg">
-                Already Have An Account ? Please{" "}
-                <Link className="text-blue-700 underline" to="/login">
-                  Login
-                </Link>{" "}
+          <div className="card flex-1 w-full ">
+            <div className=" bg-[#2A7886] rounded-2xl">
+              <h1 className="text-center mb-2 mt-2 border-b-2 w-[70%] border-[#ffff] mx-auto pb-6 text-4xl font-semibold font-dm text-white">
+                REGISTER NOW!
               </h1>
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="card-body  text-white"
+              >
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text text-white">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Enter Your Name"
+                    className="input text-black input-bordered border-[#512B58]"
+                    {...register("name", { required: true })}
+                  />
+                  {errors.name && (
+                    <span className="mt-2 text-red-800">
+                      This field is required
+                    </span>
+                  )}
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text text-white">URL</span>
+                  </label>
+                  <input
+                    type="url"
+                    name="url"
+                    placeholder="Enter Your Photo URL"
+                    className="input text-black input-bordered border-[#543A3A]"
+                    {...register("url", { required: true })}
+                  />
+                  {errors.url && (
+                    <span className="mt-2 text-red-800">
+                      This field is required
+                    </span>
+                  )}
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text text-white">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Enter Your Email"
+                    className="input text-black input-bordered border-[#543A3A]"
+                    {...register("email", { required: true })}
+                  />
+                  {errors.email && (
+                    <span className="mt-2 text-red-800">
+                      This field is required
+                    </span>
+                  )}
+                </div>
+                <div className="form-control relative">
+                  <label className="label">
+                    <span className="label-text text-white">Password</span>
+                  </label>
+                  <input
+                    type={showPass ? "text" : "password"}
+                    name="password"
+                    placeholder="Enter Your Password"
+                    className="input text-black input-bordered border-[#543A3A]"
+                    {...register("password", { required: true })}
+                  />
+                  <span
+                    onClick={handleShowPassword}
+                    className="absolute bottom-3 right-5 text-2xl cursor-pointer"
+                  >
+                    {showPass ? (
+                      <IoEye className="text-black" />
+                    ) : (
+                      <IoMdEyeOff className="text-black" />
+                    )}
+                  </span>
+                </div>
+                {errors.password && (
+                  <span className="mt-2 text-red-800">
+                    This field is required
+                  </span>
+                )}
+                <div className="form-control mt-6">
+                  <button
+                    type="submit"
+                    className="btn bg-transparent text-lg hover:bg-[#512B58] text-white transition-all"
+                  >
+                    REGISTER
+                  </button>
+                  <ToastContainer />
+                </div>
+              </form>
+              <div className="mb-5">
+                <h1 className="text-center text-white text-lg">
+                  Already Have An Account ? Please{" "}
+                  <Link className="text-blue-400 underline" to="/login">
+                    Login
+                  </Link>{" "}
+                </h1>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
