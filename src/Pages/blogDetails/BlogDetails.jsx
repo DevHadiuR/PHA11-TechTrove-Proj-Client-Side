df5e615354857c7c8878fcfd54fd5c12b032bc71
Hadiur Rahman <hadiurahman139@gmail.com>
2024-05-14 12:50:36 +0600

    added the img and long description on the details page

--- src/Pages/blogDetails/BlogDetails.jsx
+++ src/Pages/blogDetails/BlogDetails.jsx
@@ -1,6 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import NavBar from "../../Shared/navbar/NavBar";
+import { useParams } from "react-router-dom";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const BlogDetails = () => {
+  const param = useParams();
+  const id = param.id;
+
+  const axiosSecure = useAxiosSecure();
+
+  const { data } = useQuery({
+    queryFn: () => getData(),
+    queryKey: ["perBlogDetails"],
+  });
+
+  const getData = async () => {
+    const { data } = await axiosSecure(`/allBlogs/${id}`);
+
+    return data;
+  };
+
   return (
     <section className="relative z-0">
       <div className="absolute w-full top-0 z-50    bg-transparent ">
@@ -8,23 +27,25 @@ const BlogDetails = () => {
       </div>
 
       <div
-        className="hero min-h-screen"
+        className="hero min-h-[570px]"
         style={{
-          backgroundImage:
-            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+          backgroundImage: `url(${data.img})`,
         }}
       >
-        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-overlay bg-opacity-40"></div>
         <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+          <div className="max-w-2xl">
+            <h1 className="mb-5 text-3xl md:text-6xl font-bold">
+              {data.title}
+            </h1>
+            <p className="mb-5 text-xl md:text-2xl font-medium">
+              {data.shortDescription}
             </p>
           </div>
         </div>
+      </div>
+      <div className="mt-5 w-[97%] mx-auto">
+        <h1 className="text-xl font-medium">{data.longDescription}</h1>
       </div>
     </section>
   );
