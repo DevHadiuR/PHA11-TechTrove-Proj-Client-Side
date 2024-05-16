5029720ee99386e950767566732ada480a6d3368
Hadiur Rahman <hadiurahman139@gmail.com>
2024-05-16 14:11:54 +0600

    fetch all the blog data from the data base

--- src/Pages/allBlogs/AllBlogs.jsx
+++ src/Pages/allBlogs/AllBlogs.jsx
@@ -1,62 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
 import AllBLogBanner from "../../components/allBLogBanner/AllBLogBanner";
-("use client");
-import { Dropdown } from "flowbite-react";
+import AllOfTheBlogs from "../../components/allOfTheBlogs/AllOfTheBlogs";
+
+import DropdownAndSearch from "../../components/dropdown&search/DropdownAndSearch";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const AllBlogs = () => {
+  const axiosSecure = useAxiosSecure();
+
+  // fetch all the blogs
+  const { data: blogs = [] } = useQuery({
+    queryFn: () => getData(),
+    queryKey: ["allBlogs"],
+  });
+
+  const getData = async () => {
+    const { data } = await axiosSecure("/allBlogs");
+    return data;
+  };
   return (
     <section>
       <div className="relative z-0">
         <AllBLogBanner></AllBLogBanner>
       </div>
       {/* dropdown and search blog main div */}
-      <div className="mt-10 flex flex-col-reverse md:flex-row justify-between items-start md:items-center w-[90%] mx-auto ">
-        <div className="mt-3 md:mt-0  w-full md:w-auto flex md:block justify-end md:justify-start">
-          <Dropdown
-            gradientDuoTone="purpleToBlue"
-            label="Category"
-            dismissOnClick={false}
-            size="md"
-            className="rounded-xl bg-[#512B58] p-2"
-          >
-            <Dropdown.Item className="text-md text-white hover:text-black rounded-xl">
-              Latest Trends
-            </Dropdown.Item>
-            <Dropdown.Item className="text-md text-white hover:text-black rounded-xl">
-              AI and Machine Learning
-            </Dropdown.Item>
-            <Dropdown.Item className="text-md text-white hover:text-black rounded-xl">
-              Cybersecurity
-            </Dropdown.Item>
-            <Dropdown.Item className="text-md text-white hover:text-black rounded-xl">
-              Gaming
-            </Dropdown.Item>
-            <Dropdown.Item className="text-md text-white hover:text-black rounded-xl">
-              Software and Apps
-            </Dropdown.Item>
-          </Dropdown>
-        </div>
-        <div className="flex flex-col md:flex-row md:items-center gap-3">
-          <span className="text-2xl font-bold">Search Blog</span>
-          <label className="input input-primary input-bordered flex items-center gap-2">
-            <input
-              type="text"
-              className="border-none input input-bordered "
-              placeholder="Search Blogs By Blog Title"
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-5 h-5 opacity-70 cursor-pointer hover:text-primary hover:scale-125 transition-all"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </label>
-        </div>
+      <div>
+        <DropdownAndSearch></DropdownAndSearch>
+      </div>
+      {/* all of the blogs */}
+      <div className="mt-14 ">
+        {blogs.map((blog) => (
+          <AllOfTheBlogs key={blog._id} blog={blog}></AllOfTheBlogs>
+        ))}
       </div>
     </section>
   );
---
+++ src/components/allOfTheBlogs/AllOfTheBlogs.jsx
@@ -0,0 +1,83 @@
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+const AllOfTheBlogs = ({ blog }) => {
+  return (
+    <div key={blog._id} className="mt-8">
+      <div className="container grid grid-cols-12 mx-auto bg-[#F1F3F4] rounded-xl">
+        <div
+          className="bg-no-repeat bg-gray-400 md:bg-transparent  bg-cover  col-span-full md:col-span-5 row-span-full"
+          style={{
+            backgroundImage: `url(${blog.img})`,
+            backgroundPosition: "center center",
+            backgroundBlendMode: "multiply",
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="flex flex-col p-6 col-span-full row-span-full md:col-span-7 lg:p-10 ml-0 md:ml-8">
+          <div className="flex justify-start">
+            <span
+              className={`px-3 py-2 mb-5 rounded-full
+                     ${
+                       blog.category === "Latest Trends" &&
+                       "text-blue-600 bg-blue-100/90"
+                     } ${
+                blog.category === "AI and Machine Learning" &&
+                "text-green-600 bg-green-100/90"
+              } ${
+                blog.category === "Cybersecurity" &&
+                "text-red-600 bg-red-100/90"
+              } ${
+                blog.category === "Gaming" && "text-yellow-600 bg-yellow-100/90"
+              } ${
+                blog.category === "Software and Apps" &&
+                "text-purple-600 bg-purple-100/90"
+              } `}
+            >
+              {blog.category}
+            </span>
+          </div>
+          <h1 className="text-3xl text-white opacity-90 md:text-black md:opacity-100 font-semibold ">
+            {blog.title}
+          </h1>
+          <p className="flex-1 pt-2 text-white opacity-90 md:text-black md:opacity-100">
+            {blog.shortDescription}
+          </p>
+          <a
+            rel="noopener noreferrer"
+            href="#"
+            className="inline-flex items-center pt-2 pb-6 space-x-2 text-sm text-violet-400 text-white  md:text-black "
+          >
+            <Link to="/blogDetails">
+              <span className="text-white opacity-90 md:text-black md:opacity-100">
+                Read more
+              </span>
+            </Link>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-4 h-4"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </a>
+          <div className="flex items-center gap-5 pt-2">
+            <Link to={`/blogDetails/${blog._id}`}>
+              <Button outline gradientDuoTone="purpleToBlue">
+                Details
+              </Button>
+            </Link>
+            <Button gradientDuoTone="purpleToBlue">Wish List</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllOfTheBlogs;
