4b193cbc7d04bb50fbc20c8e3bb3e155942de902
Hadiur Rahman <hadiurahman139@gmail.com>
2024-05-16 13:47:35 +0600

    added the dropdown and search filter design in allblogs page

--- src/components/allBLogBanner/AllBLogBanner.jsx
+++ src/components/allBLogBanner/AllBLogBanner.jsx
@@ -16,8 +16,8 @@ const AllBLogBanner = () => {
         <div className="hero-overlay bg-opacity-40"></div>
         <div className="hero-content text-center text-neutral-content mt-28">
           <div className="max-w-4xl">
-            <h1 className="mb-5 text-3xl max-w-xl mx-auto  md:text-5xl font-bold text-white opacity-85">
-              Explore Our World of Tech Updates
+            <h1 className="mb-5 text-3xl max-w-2xl mx-auto  md:text-5xl font-bold text-white opacity-85">
+              Explore Our All the News & Updates of Gaming
             </h1>
             <p className="mb-5 text-xl md:text-2xl font-medium opacity-90">
               Welcome to the universe of tech in here, your portal to the latest
